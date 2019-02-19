a6e43daa1417d0f1fc267ad910292d238139d715
Jose Guerra <jose.guerra@finalsite.com>
2019-02-19 22:35:05 +0000

    customized build output

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,7 +1,84 @@
+const path = require("path")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin")
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
+
 module.exports = {
   entry: {
-    main: ""
+    main: ["./src/index.js"]
   },
-  mode: "",
-  output: {}
+  mode: "production",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: ""
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCSSExtractPlugin.loader,
+            options: {
+              publicPath: '../dist/css/'
+            }
+          },
+          { loader: "css-loader" },
+          { loader: "postcss-loader" },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCSSExtractPlugin({
+      filename: "css/[name]-[contenthash].css"
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano"),
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+      canPrint: true
+    }),
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      inject: true
+    }),
+    new UglifyJSPlugin()
+  ]
 }
