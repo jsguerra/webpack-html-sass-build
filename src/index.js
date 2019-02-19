ebf2af1df0809490854e663dbe06decd4a8583ca
Jose Guerra <jose.guerra@finalsite.com>
2019-02-19 22:35:47 +0000

    added test js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 // Javascript goes here
-import style from "./scss/main.scss";
+// import style from "./scss/main.scss";
+require("babel-runtime/regenerator")
+require("./scss/main.scss")
+// require("./images/link.jpg")
+require("./index.html")
+
+console.log('hello world')
