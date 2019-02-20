c59d39bdd12a266d9e1eba03c6ac05c0bcbb901f
Jose Guerra <jose.guerra@finalsite.com>
2019-02-20 08:55:18 +0000

    removed unnecessary statements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// Javascript goes here
-// import style from "./scss/main.scss";
-require("babel-runtime/regenerator")
 require("./scss/main.scss")
 // require("./images/link.jpg")
 require("./index.html")
